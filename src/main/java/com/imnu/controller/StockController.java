7a4b60be3796c9218727de5b9b809bdfe9a64544
LU-0627 <z878150240@gmail.com>
2022-06-26 17:28:32 +0800

    库存的分页和模糊查询

--- src/main/java/com/imnu/controller/StockController.java
+++ src/main/java/com/imnu/controller/StockController.java
@@ -1,17 +1,63 @@
 package com.imnu.controller;
 
+import com.github.pagehelper.PageInfo;
+import com.imnu.bean.VO.StockVO;
 import com.imnu.service.StockService;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * @author WenWangXin
  * @create 2022-06-25-2:27
  */
+@Controller
+@RequestMapping("/stock")
 public class StockController {
 
     @Resource
     @Qualifier("StockServiceImpl")
     private StockService stockService;
+
+    /**
+     * 分页带查询功能
+     * @param page
+     * @param size
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "getPage", method = RequestMethod.GET)
+    public String getPage(@RequestParam(defaultValue = "1") int page,
+                          @RequestParam(defaultValue = "5") int size,
+                          Model model) {
+        List<StockVO> pageList = stockService.getPage(page, size);
+        PageInfo pageInfo = new PageInfo(pageList);
+        model.addAttribute("pageInfo", pageInfo);
+        return "pageInfo";
+    }
+
+    /**
+     * 模糊查询
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "query", method = RequestMethod.GET)
+    public String query(Model model, HttpServletRequest request) {
+        String stockName=request.getParameter("stockName");
+        List<StockVO> stocks = stockService.query(stockName);
+        model.addAttribute("stocks", stocks);
+        return "";
+    }
+
+/*    @RequestMapping(value = "update",method = RequestMethod.POST)
+    public String update(Model model){
+
+    }*/
 }
--- src/main/java/com/imnu/service/impl/StockServiceImpl.java
+++ src/main/java/com/imnu/service/impl/StockServiceImpl.java
@@ -1,16 +1,40 @@
 package com.imnu.service.impl;
 
+import com.github.pagehelper.PageHelper;
+import com.imnu.bean.Stock;
+import com.imnu.bean.VO.StockVO;
 import com.imnu.mapper.StockMapper;
+import com.imnu.service.StockService;
+
+import java.util.List;
 
 /**
  * @author WenWangXin
  * @create 2022-06-25-1:10
  */
-public class StockServiceImpl {
+public class StockServiceImpl implements StockService {
 
     private StockMapper stockMapper;
 
     public void setStockMapper(StockMapper stockMapper) {
         this.stockMapper = stockMapper;
+    }
+
+    /**
+     * 获取分页查询全部数据
+     *
+     * @param page
+     * @param size
+     * @return
+     */
+    @Override
+    public List<StockVO> getPage(int page, int size) {
+        PageHelper.startPage(page, size);
+        return stockMapper.getPage();
+    }
+
+    @Override
+    public List<StockVO> query(String sockName) {
+        return stockMapper.query(sockName);
     }
 }
