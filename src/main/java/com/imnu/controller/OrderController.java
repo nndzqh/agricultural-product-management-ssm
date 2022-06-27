e436eb1a46bd29779d03b0702746e86112bd6620
shenchao <shenchao@imnu.cn>
2022-06-27 15:08:03 +0800

    对分页的完善

--- src/main/java/com/imnu/controller/OrderController.java
+++ src/main/java/com/imnu/controller/OrderController.java
@@ -72,7 +72,7 @@ public class OrderController {
     public String getPage(@RequestParam(defaultValue = "1") int page,
                           @RequestParam(defaultValue = "5")int size, Model model, String keyWord){
         List<Orders> pageList = ordersService.getPage(page,size,keyWord);
-       // System.out.println("EEEEEEEEEEEEE"+keyWord);
+       //  System.out.println("EEEEEEEEEEEEE"+keyWord);
         List<OrdersVo> ordersVoList = pageList.stream().map((item) ->{
             OrdersVo ordersVo = new OrdersVo();
             BeanUtils.copyProperties(item, ordersVo);
